7f0c0cace09224050a9353845a6be6c206b7c734
KammaleshSJ-4 <133968318+KammaleshSJ-4@users.noreply.github.com>
2026-08-03 22:05:22 +0530

    Create procedure to load data into bronze schema
    Added a procedure to load data from CSV files into the bronze schema, including truncating existing tables and bulk inserting new data.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -3,76 +3,79 @@
 -- LOADING ALL THE VALUES IN CSV FILES INTO THE DATABASES 
 -- ===================================================================================
 
--- -----------------------------------------------------------------------------------
---				CRM SOURCE 
--- -----------------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	-- -----------------------------------------------------------------------------------
+	--				CRM SOURCE 
+	-- -----------------------------------------------------------------------------------
 
-TRUNCATE TABLE bronze.crm_cust_info;
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-BULK INSERT bronze.crm_cust_info 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-TRUNCATE TABLE bronze.crm_prd_info;
-
-BULK INSERT bronze.crm_prd_info 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	BULK INSERT bronze.crm_cust_info 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
-TRUNCATE TABLE bronze.crm_sales_details;
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-BULK INSERT bronze.crm_sales_details 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
--- --------------------------------------------------------------------------------------
---				ERP SOURCE 
--- --------------------------------------------------------------------------------------
-
-TRUNCATE TABLE bronze.erp_cust_az12;
-
-BULK INSERT bronze.erp_cust_az12 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	BULK INSERT bronze.crm_prd_info 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
+	TRUNCATE TABLE bronze.crm_sales_details;
 
-TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.crm_sales_details 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.erp_loc_a101 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	-- --------------------------------------------------------------------------------------
+	--				ERP SOURCE 
+	-- --------------------------------------------------------------------------------------
+
+	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	BULK INSERT bronze.erp_cust_az12 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	TRUNCATE TABLE bronze.erp_loc_a101;
 
-BULK INSERT bronze.erp_px_cat_g1v2 
-FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	BULK INSERT bronze.erp_loc_a101 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+
+
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	BULK INSERT bronze.erp_px_cat_g1v2 
+	FROM 'D:\Projects\sql_data_warehouse\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+END
