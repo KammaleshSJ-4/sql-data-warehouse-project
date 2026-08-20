7793520d53cf95f2c692d0e98e1ee530e532942f
KammaleshSJ-4 <133968318+KammaleshSJ-4@users.noreply.github.com>
2026-08-20 20:44:50 +0530

    Add silver.crm_prd_info table and data insertion
    Created the silver.crm_prd_info table with corrected schema and inserted data from bronze.crm_prd_info.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -37,3 +37,51 @@ WHERE flag_last = 1;
 SELECT * FROM silver.crm_cust_info;
 
 
+-- silver.crm_prd_info Table
+
+-- Creation of Table with correction in column schema
+IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info
+CREATE TABLE silver.crm_prd_info (
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(100),
+	prd_nm NVARCHAR(200),
+	prd_cost INT,
+	prd_line NVARCHAR(100),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
+
+-- Inserting into the table
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+	SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost,0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END	as prd_line,
+	CAST(prd_start_dt AS DATE) as prd_start_dt,
+	CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+
+SELECT * from silver.crm_prd_info;
+
+
