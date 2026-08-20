1d9056766d3a3eb61c2c19d2bcbc40981e416839
KammaleshSJ-4 <133968318+KammaleshSJ-4@users.noreply.github.com>
2026-08-20 22:07:07 +0530

    Create and populate crm_sales_details table

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -85,3 +85,67 @@ FROM bronze.crm_prd_info;
 SELECT * from silver.crm_prd_info;
 
 
+
+-- crm_sales_details table
+
+
+-- Dropping and creation of table if exist with changes
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+);
+
+-- Insrting the values 
+INSERT INTO silver.crm_sales_details (
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		sls_order_dt,
+		sls_ship_dt,
+		sls_due_dt,
+		sls_sales,
+		sls_quantity,
+		sls_price)
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id, 
+	CASE											--  Converrt format into date
+		WHEN sls_order_dt= 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR(100)) AS DATE)
+	END as sls_order_dt,
+	CASE 
+		WHEN sls_ship_dt= 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR(100)) AS DATE)
+	END as sls_ship_dt,
+	CASE 
+		WHEN sls_due_dt= 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR(100)) AS DATE)
+	END as sls_due_dt,
+	CASE
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END as sls_sales,									-- Derive sales from price and quantity
+	sls_quantity,
+	CASE
+		WHEN sls_price <=0 THEN ABS(sls_price)
+		WHEN sls_price IS NULL THEN ABS(sls_sales) / sls_quantity
+		ELSE sls_price
+	END as sls_price									-- Derive missing price with sales and quantity
+
+FROM bronze.crm_sales_details;
+
+SELECT * FROM silver.crm_sales_details;
