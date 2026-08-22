50dfdd8dfe46d1e6d0ac521fd97498bed80c4a21
KammaleshSJ-4 <133968318+KammaleshSJ-4@users.noreply.github.com>
2026-08-22 23:00:40 +0530

    Add view for sales data aggregation
    Created a new view 'fact_sales' to aggregate sales data.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -40,3 +40,23 @@ FROM silver.crm_prd_info pi
 LEFT JOIN silver.erp_px_cat_g1v2 pe
 ON pi.cat_id = pe.id
 WHERE pi.prd_end_dt IS NULL;  -- Filter out all historcal data
+
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+	so.sls_ord_num AS order_number,
+	dp.product_number AS product_number,
+	dc.customer_id AS customer_id,
+	so.sls_order_dt AS order_date,
+	so.sls_ship_dt AS shipping_date,
+	so.sls_due_dt AS due_date,
+	so.sls_sales AS sales_amount,
+	so.sls_quantity AS quantity,
+	so.sls_price AS price
+FROM silver.crm_sales_details so
+LEFT JOIN gold.dim_product dp
+	on so.sls_prd_key = dp.product_number
+LEFT JOIN gold.dim_customers dc
+	on so.sls_cust_id = dc.customer_id
+
+SELECT * FROM gold.fact_sales
